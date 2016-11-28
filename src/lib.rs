65105a8f7a0fc4fc6768225f11a1a7289e873043
Bernhard Leiner <bleiner@gmail.com>
2016-11-28 23:08:11 +0100

    use byteorder instead of unsafe transmute

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,13 @@ use wtree::{DistanceAndTime};
 #[macro_use]
 extern crate log;
 
+extern crate byteorder;
+use byteorder::{LittleEndian, WriteBytesExt};
+
 // constants
 pub const FIT_FILE_HDR_SIZE: usize = 14;
 
 #[derive(Clone, Debug)]
-#[repr(packed)]
 pub struct FitFileHeader {
     header_size: u8,
     protocol_version: u8,
@@ -28,11 +30,11 @@ pub struct FitFileHeader {
 impl FitFileHeader {
     pub fn new() -> FitFileHeader {
         let fit_protocol_version_major_shift = 4;
-        let fit_protocol_version_20 = 2 << fit_protocol_version_major_shift;
+        let fit_protocol_version_10 = 1 << fit_protocol_version_major_shift;
 
         FitFileHeader {
             header_size: FIT_FILE_HDR_SIZE as u8,
-            protocol_version: fit_protocol_version_20,
+            protocol_version: fit_protocol_version_10,
             profile_version: 2,
             data_size: 0,
             data_type: [46, 70, 73, 84], // ".FIT"
@@ -40,8 +42,15 @@ impl FitFileHeader {
         }
     }
 
-    pub fn bin(&self) -> [u8; FIT_FILE_HDR_SIZE] {
-        unsafe { mem::transmute(self.clone()) }
+    pub fn bin(&self) -> Vec<u8> {
+        let mut wtr = vec![];
+        wtr.write_u8(self.header_size).unwrap();
+        wtr.write_u8(self.protocol_version).unwrap();
+        wtr.write_u16::<LittleEndian>(self.profile_version).unwrap();
+        wtr.write_u32::<LittleEndian>(self.data_size).unwrap();
+        wtr.extend(self.data_type.iter().cloned());
+        wtr.write_u16::<LittleEndian>(self.crc).unwrap();
+        wtr
     }
 
     pub fn calc_crc(&mut self) {
@@ -50,13 +59,28 @@ impl FitFileHeader {
     }
 }
 
+pub trait DataRecord {
+    fn header_byte(&self) -> Vec<u8>;
+}
+
+pub trait FITMessage {
+    fn definition_message(&self) -> Vec<u8>;
+    fn data_message(&self) -> Vec<u8>;
+    fn bin(&self) -> Vec<u8>;
+    // TODO create default implementation for bin
+}
+
+
+
+
+
+
 pub fn simple_parse(input: String) {
     let w = jd_grammar::parse_Workout_main(&parse::preprocess_input(&input)).unwrap();
     info!("{}", input);
     info!("({:.*} km, {}:{:02} h)", 1, w.distance() as f32 / 1000.0,
           w.time() as i32 / 3600, w.time() as i32 % 3600 / 60);
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -65,8 +89,14 @@ mod tests {
 
     #[test]
     fn fit_file_header_size() {
-        assert_eq!(FIT_FILE_HDR_SIZE, mem::size_of::<FitFileHeader>());
-        assert_eq!(FitFileHeader::new().header_size as usize,
-                   mem::size_of::<FitFileHeader>());
+        let fh = FitFileHeader::new();
+        assert_eq!(FIT_FILE_HDR_SIZE, fh.bin().len());
+    }
+
+    #[test]
+    fn file_header() {
+        let mut fh = FitFileHeader::new();
+        fh.data_size = 360;
+        println!("{:?}", fh.bin());
     }
 }
