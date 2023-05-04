56d86d10ddeda85409ea056c3fec51c0e2af22ce
Bernhard Leiner <bleiner@gmail.com>
2023-05-04 22:31:42 +0200

    add cli integration test

---
+++ tests/cli.rs
@@ -0,0 +1,13 @@
+use assert_cmd::Command;
+
+#[test]
+fn cli_workout() -> () {
+    let mut cmd = Command::cargo_bin("workout").unwrap();
+    cmd.arg("-c")
+        .arg("tests/paces.toml")
+        .arg("-w")
+        .arg("10E + 3 * (2 min I + 2 min rst)");
+    cmd.assert()
+        .success()
+        .stdout("11.9 km, 1:08 h, 5:45 min/km\n");
+}
