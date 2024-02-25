92cce73a7025133649ce61d6c0dcc5e67ec2dd4e
Bernhard Leiner <bleiner@gmail.com>
2024-02-25 21:47:08 +0100

    [egui] add option to delete configuration items

--- src/egui.rs
+++ src/egui.rs
@@ -46,8 +46,6 @@ impl WorkoutApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             config: HashMap::from([
-                ("rst".to_owned(), pace2speed("15:00")),
-                ("jg".to_owned(), pace2speed("8:00")),
                 ("E".to_owned(), pace2speed("5:40")),
                 ("M".to_owned(), pace2speed("5:00")),
                 ("T".to_owned(), pace2speed("4:30")),
@@ -70,18 +68,25 @@ fn paces_to_strings(input: &HashMap<String, f32>) -> HashMap<String, String> {
 impl eframe::App for WorkoutApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut remove_config: String = "".to_owned();
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.vertical(|ui| {
                     ui.heading("Paces");
                     for (k, v) in self.config.iter_mut() {
-                        ui.add(
-                            egui::Slider::new(v, 1.0..=8.0)
-                                .text(k)
-                                .custom_formatter(|n, _| speed2pace(n as f32))
-                                .custom_parser(|s| Some(f64::from(pace2speed(s))))
-                                .trailing_fill(true),
-                        );
+                        ui.horizontal(|ui| {
+                            if ui.button("🗙").clicked() {
+                                remove_config = k.clone(); // schedule for removal
+                            }
+                            ui.add(
+                                egui::Slider::new(v, 1.0..=8.0)
+                                    .text(k)
+                                    .custom_formatter(|n, _| speed2pace(n as f32))
+                                    .custom_parser(|s| Some(f64::from(pace2speed(s))))
+                                    .trailing_fill(true),
+                            );
+                        });
                     }
                 });
                 ui.vertical(|ui| {
@@ -96,5 +101,10 @@ impl eframe::App for WorkoutApp {
                 });
             });
         });
+
+        // processing
+        if self.config.contains_key(&remove_config) {
+            self.config.remove(&remove_config);
+        }
     }
 }
